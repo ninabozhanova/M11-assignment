009c5a14062f855702eddb29b504252b82c101b2
ninabozhanova <nbozhanova@gmail.com>
2024-05-20 22:18:47 -0700

    done with numbers

--- js/math.js
+++ js/math.js
@@ -1,9 +1,36 @@
 //STEP 1
+let userNumber = parseInt(window.prompt('Please enter a number'))
+if (!isNaN(userNumber)) {
+    console.log(Math.abs(userNumber))
+} else {
+    window.alert(`Please enter a valid number`)
+}
 
 //STEP 2
+// let userNumber = parseFloat(window.prompt('Please enter a decimal'))
+// if (!isNaN(userNumber)) {
+//     console.log(Math.ceil(userNumber))
+// } else {
+//     window.alert(`Please enter a valid decimal`)
+// }
 
 //STEP 3
+// let userNumber = parseFloat(window.prompt('Please enter a decimal'))
+// if (!isNaN(userNumber)) {
+//     console.log(Math.floor(userNumber))
+// } else {
+//     window.alert(`Please enter a valid decimal`)
+// }
 
 //STEP 4
+// let numbers = window.prompt('Please enter 5 comma-separated numbers').split(',').map(Number)
+// console.log(`The largest number is ${Math.max(...numbers)}`)
+// console.log(`The smallest number is ${Math.min(...numbers)}`)
 
 //STEP 5
+// let userNumber = parseInt(window.prompt('Please enter a number'))
+// if (!isNaN(userNumber) && userNumber > 0) {
+//     console.log(Math.sqrt(userNumber))
+// } else {
+//     window.alert(`Please enter a valid number`)
+// }
